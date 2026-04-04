bdb6e3facb851f777b639a04922917c8ecd899ae
ollycohenzofficial-sudo <ollycohenzofficial@gmail.com>
2026-04-04 18:35:20 +0300

    Add global configuration variables for bot

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
+global.ownername = "Musaasizi Marvin";
+global.botname = "Cohenz Pro Bot";
+global.sudo = ["256709913725"];
+global.packname = "Cohenz Pro";
+global.author = "iMac Recordz";
+global.prefix = ".";
+global.gemini_api_key = process.env.GEMINI_API_KEY;
+global.gemini_model = "gemini-1.5-flash"; 
 require('dotenv').config();
 
 global.APIs = {
