bb6b214761f65b1f68d9e41dbbc4bfb7ce1fe866
ollycohenzofficial-sudo <ollycohenzofficial@gmail.com>
2026-04-13 20:28:19 +0300

    Cohenz Pro Bot

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Cohenz Pro Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Knight Bot', // Your name
+  botName: "Cohenz Pro Bot",
+  botOwner: 'Musaasizi Marvin', // Your name
   ownerNumber: '256709913725', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/ollycohenzofficial-sudo/Knightbot-MD2006/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
