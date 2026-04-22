4fe9c1347b1673a5b34e77efd5214bfbb9238ac5
ollycohenzofficial-sudo <ollycohenzofficial@gmail.com>
2026-04-22 18:22:09 +0300

    Update author and bot owner details in settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,24 @@
 const settings = {
   packname: 'Cohenz Pro Bot',
-  author: 'Marvin Musaasiz‎',
+  author: 'Marvin Musaasiz',
   botName: "Cohenz Pro Bot",
-  botOwner: 'Musaasizi Marvin', // Your name
-  ownerNumber: '256709913725', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Musaasizi Marvin',
+  ownerNumber: '256709913725', 
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/ollycohenzofficial-sudo/Knightbot-MD2006/archive/refs/heads/main.zip",
 };
+
+// Global variables for plugin compatibility
+global.packname = settings.packname
+global.author = settings.author
+global.botname = settings.botName
+global.ownername = settings.botOwner
+global.owner = [settings.ownerNumber]
+global.prefix = "." 
+global.public_mode = settings.commandMode === "public" ? true : false
 
 module.exports = settings;
