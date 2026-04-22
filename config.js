e9412ef3333239e00d45c5a52ba10ee137fbc599
ollycohenzofficial-sudo <ollycohenzofficial@gmail.com>
2026-04-22 08:52:23 +0300

    Refactor config.js for API keys and global variables

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); 
+require('dotenv').config(); // MUST BE LINE 1
 global.ownername = "Musaasizi Marvin";
 global.botname = "Cohenz Pro Bot";
 global.sudo = ["256709913725"];
@@ -6,31 +6,23 @@ global.owner = ["256709913725"];
 global.packname = "Cohenz Pro";
 global.author = "iMac Recordz";
 global.prefix = ".";
-global.gemini_api_key = process.env.GEMINI_API_KEY; 
+
+// AI API KEYS
+global.gemini_api_key = process.env.GEMINI_API_KEY;
 global.suno_api_key = process.env.SUNO_API_KEY;
 global.gemini_model = "gemini-1.5-flash";
 
 global.APIs = {
   xteam: 'https://api.xteam.xyz',
-  dzx: 'https://api.dhamzxpath.my.id',
   lol: 'https://api.lolhuman.xyz',
-  violetics: 'https://violetics.pw',
   neoxr: 'https://api.neoxr.my.id',
   zenzapis: 'https://zenzapis.xyz',
   akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',
-  nrtm: 'https://fg-nrtm.ddns.net',
-  bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
 };
 
 global.APIKeys = {
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': 'yourkey',
-  'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': 'yourkey',
-  'https://api-fgmods.ddns.net': 'fg-dylux'
 };
 
 module.exports = {
@@ -38,4 +30,4 @@ module.exports = {
   APIs: global.APIs,
   APIKeys: global.APIKeys
 };
-      
+
